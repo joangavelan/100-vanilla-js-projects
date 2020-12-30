06b272f3ccdd8bfde5782e4753ba252920b299ec
Johan Gavelan <johangavelan@gmail.com>
2020-12-29 20:39:53 -0500

    project done

---
+++ carousel-modal/app.js
@@ -0,0 +1,52 @@
+const App = (() => {
+    const images = document.querySelectorAll('img');
+    const modalEl = document.querySelector('.modal');
+    const modalImageEl = document.querySelector('.modal__image');
+    const switchIcons = document.getElementsByClassName('switch');
+    
+    const openModal = image => {
+        modalEl.classList.add('active');
+        modalImageEl.innerHTML = image;
+    }
+
+    const closeModal = () => {
+        const closingClasses = ['modal', 'close'];
+        window.addEventListener('click', event => {
+            if(closingClasses.some(closingClass => event.target.classList.contains(closingClass))) {
+                modalEl.classList.remove('active')
+            }
+        })
+    }
+
+    const switchImage = index => {
+        for(let icon of switchIcons) {
+            icon.addEventListener('click', () => {
+                if(icon.classList.contains('next')) {
+                    index < (images.length - 1) ? index++ : index = 0;
+                }
+                if(icon.classList.contains('previous')) {
+                    index > 0 ? index-- : index = (images.length - 1);
+                }
+            modalImageEl.innerHTML = images[index].outerHTML;
+            })
+        }
+    }
+
+    const carousel = () => {
+        images.forEach((image, index) => {
+            image.addEventListener('click', function() {
+                const clickedImage = this.outerHTML; 
+                openModal(clickedImage);
+                switchImage(index);
+                closeModal();
+            })
+        })
+    }
+    
+    
+    return {
+        carousel
+    }
+})();
+
+App.carousel();
