a18b9161f781fb2249c84f769713b651f12e5531
Johan Gavelan <johangavelan@gmail.com>
2021-01-07 20:28:09 -0500

    change color background submodule done

--- change-color-backgroundd/js/script.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('button');
-const body = document.querySelector('body');
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
-
-const randomColor = _ => colors[Math.floor(Math.random() * colors.length)];
-
-button.addEventListener('click', () => 
-    body.style.backgroundColor = randomColor()
-);
