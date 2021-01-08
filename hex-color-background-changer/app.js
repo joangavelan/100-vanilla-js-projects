47767eed7c9a24dee5d4113a42f22f1ed90a22fc
Johan Gavelan <johangavelan@gmail.com>
2021-01-07 22:11:33 -0500

    refactored

--- hex-color-background-changer/app.js
+++ hex-color-background-changer/app.js
@@ -1,5 +1,5 @@
 const hexEl = document.querySelector('#hex-value');
-const button = document.querySelector('.btn');
+const button = document.querySelector('button');
 const body = document.querySelector('body');
 const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
 
